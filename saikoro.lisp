dc944f6018aa214693c7343cb66cc4a6116d0eb4
David Lindelöf <david.lindelof@neurobat.net>
2011-05-13 17:00:20 +0200

    Starting afresh

--- saikoro.lisp
+++ saikoro.lisp
@@ -1,53 +1,9 @@
-(defparameter *sample-board*
-  '((5 1 3 3 1 2 2 6)
-    (1 1 3 6 3 1 5 3)
-    (1 2 5 2 6 5 4 4)
-    (3 6 3 3 4 3 5 5)
-    (4 5 3 1 2 6 3 4)
-    (4 6 6 4 3 6 4 6)
-    (3 1 4 3 5 2 5 1)
-    (6 4 5 6 6 3 1 5))
-  "A sample board.")
+(defparameter *board-size* 6)
+(defparameter *board-squares-num* (* *board-size* *board-size*))
 
-(defstruct square
-  "Representation of a square"
-  value
-  (neighbors ())
-  (dof ()))
+(defun board-array (lst)
+  (make-array *board-squares-num* :initial-contents lst))
 
-(defun mappend (fn the-list)
-  "Apply fn to each element of list and append the result."
-  (if (null the-list)
-      nil
-      (append (funcall fn (first the-list))
-              (mappend fn (rest the-list)))))
-
-(defun remove-square (square)
-  "Remove a square from the game"
-  (let ((neighbors (square-neighbors square)))
-    (dolist (neighbor neighbors)
-      (delete square (square-neighbors neighbor)))
-    (dolist (neighbor neighbors)
-      (update-dof neighbor))))
-
-(defun find-dof (square &optional (depth 0))
-  "Find squares that can be reached from this square."
-  (cond ((= depth 6) nil)
-        ((append (choose-if #'(lambda (square) (= (square-value square) (+1 depth)))
-                            (square-neighbors square))
-                 (mappend #'(lambda (square) (find-dof square (+ depth 1)))
-                          (square-neighbors square))))))
-
-(defun parse-board (board &optional (row-nr 8))
-  "Parses a board and returns a list of (row col square)s."
-  (when (not (null (first board)))
-    (append (parse-row (first board) row-nr)
-            (parse-board (rest board) (- row-nr 1)))))
-
-(defun parse-row (row row-nr &optional (col-nr 1))
-  (when (not (null (first row)))
-    (append (list (list row-nr col-nr (make-square :value (first row))))
-            (parse-row (rest row) row-nr (+ 1 col-nr)))))
-
-(defun find-neighbors (parsed-board)
-  (mapcar 
+(defun gen-board ()
+  (board-array (loop for n below *board-squares-num*
+                    collect (1+ (random 6)))))
